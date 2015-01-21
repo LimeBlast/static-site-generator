d44c1bf1874a71343563730605d8f8a0175c64f9
Daniel Hollands <daniel@limeblast.co.uk>
2015-01-21 22:49:44 +0000

    added jekyll-assets and pulled in uikit via bower

---
+++ app/_assets/javascripts/app.js
@@ -0,0 +1,6 @@
+//= require jquery/dist/jquery.js
+//= require uikit/js/uikit.js
+
+$(function () {
+
+});
---
+++ app/_plugins/ext.rb
@@ -0,0 +1,4 @@
+require "jekyll-assets"
+require "sprockets"
+
+Sprockets.append_path "bower_components"
