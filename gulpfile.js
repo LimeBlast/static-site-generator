9f53730f44f2ec24e012523c046c7c3f25543f83
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-02-04 12:49:23 +0000

    making some progress, but there is still a long way to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
-var gulp = require('gulp');
+"use strict";
 
-gulp.task('default', function () {
-  // place code for your default task here
+var gulp = require('gulp')
+  , shell = require("gulp-shell")
+  , ghpages = require('gh-pages')
+  , browserSync = require('browser-sync')
+  , options;
+
+options = {
+  jekyllServer: 'localhost:4000',
+  dist: './dist/'
+};
+
+gulp.task("jekyll:serve", shell.task("jekyll serve"));
+gulp.task("jekyll:build:watch", shell.task("jekyll build --watch"));
+
+gulp.task("deploy", function (cb) {
+  ghpages.publish(options.dist, {}, cb);
 });
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    proxy: options.jekyllServer
+  });
+});
+
+gulp.task("default", ["browser-sync", "jekyll:serve"]);
